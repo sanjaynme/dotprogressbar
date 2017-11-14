ee4b8379e4e9088977ad5840477623a8379eb88f
sanjul910bm <sanju.l910bm@gmail.com>
2017-11-14 17:29:11 +0545

    added dots

---
+++ app/src/main/java/learn/ebpearls/com/splashscreen/AnimationListener.java
@@ -0,0 +1,18 @@
+package learn.ebpearls.com.splashscreen;
+
+import android.view.animation.Animation;
+
+/**
+ * @author Silvestr Predko.
+ */
+abstract class AnimationListener implements Animation.AnimationListener {
+    @Override
+    public void onAnimationStart(Animation animation) {
+        // stub
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        // stub
+    }
+}
